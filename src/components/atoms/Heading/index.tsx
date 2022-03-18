343edb648815180ab1b8b79c64975f9965d0df66
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-18 18:35:31 -0300

    fix(heading): export styled h1 direct

--- src/components/atoms/Heading/index.tsx
+++ src/components/atoms/Heading/index.tsx
@@ -1,14 +1,3 @@
-import type { ReactNode } from 'react';
 import styled from 'styled-components';
 
-export default Heading;
-
-type Props = {
-   children: ReactNode;
-};
-
-function Heading({ children }: Props) {
-   return <HeadingComponent>{children}</HeadingComponent>;
-}
-
-const HeadingComponent = styled.h1``;
+export default styled.h1``;
