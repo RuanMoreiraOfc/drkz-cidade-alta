285fd938a5947cceed0f338929c6957153f64fb3
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-17 21:18:20 -0300

    feat: create `Dashboard` page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default Dashboard;
+
+type Props = {};
+
+function Dashboard({}: Props) {
+   return <Page>Você só pode entrar aqui com permissão</Page>;
+}
+
+const Page = styled.div``;
