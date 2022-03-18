fa810ad1bbeb1ad6775a2d65491df8a0d80b77d1
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-17 21:22:11 -0300

    refactor: make `MyApp` router only

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
+import { GlobalStyle } from '@styles/Globals';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
 import MyApp from '@pages/_app';
 
+const Page = styled.div`
+   max-width: 100%;
+   min-height: 100vh;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+`;
+
 ReactDOM.render(
    <React.StrictMode>
-      <MyApp />
+      <Page>
+         <GlobalStyle />
+         <MyApp />
+      </Page>
    </React.StrictMode>,
    document.getElementById('root'),
 );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,20 @@
-import { GlobalStyle } from '@styles/Globals';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
 
 import Home from '@pages/index';
 
 export default MyApp;
 
-function MyApp() {
-   return (
-      <Page>
-         <GlobalStyle />
+type Props = {};
 
-         <BrowserRouter>
-            <Routes>
-               <Route path='/' element={<Home />} />
-            </Routes>
-         </BrowserRouter>
-      </Page>
+function MyApp({}: Props) {
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route
+               path='/' //
+               element={<Home />}
+            />
+         </Routes>
+      </BrowserRouter>
    );
 }
-
-const Page = styled.div`
-   max-width: 100%;
-   min-height: 100vh;
-
-   display: flex;
-   align-items: center;
-   justify-content: center;
-`;
