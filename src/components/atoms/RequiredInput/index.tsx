96480da4c8750a9a26468b3388245407e514919d
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-20 21:16:08 -0300

    feat: create `RequiredInput` atom

---
+++ src/components/atoms/RequiredInput/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.input.attrs({
+   required: true,
+})`
+   width: 100%;
+   padding: 0.8rem;
+
+   border: none;
+   border-radius: 0.4rem;
+
+   font-size: 1.8rem;
+`;
