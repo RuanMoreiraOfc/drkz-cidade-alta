52e053ce48d329aebae19b08bf8fdb6697d02283
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-22 18:45:32 -0300

    chore: mock api data in dashboard test

--- test/pages/dashboard.test.tsx
+++ test/pages/dashboard.test.tsx
@@ -1,10 +1,62 @@
-import { describe, it, expect } from 'vitest';
+import { vi, describe, beforeEach, afterEach, it, expect } from 'vitest';
 
 import { createPageContainer } from '@t-utils/domManipulator';
+
+import type { Routes } from '@services/api';
+import api from '@services/api';
 
 import Dashboard from '@pages/dashboard';
 
 describe('Dashboard', () => {
+  const codigoPenalApiData = [
+    {
+      id: 1,
+      nome: 'B',
+      dataCriacao: '2021-04-29T01:26:35.700Z',
+      multa: 10,
+      status: 1,
+    },
+    {
+      id: 2,
+      nome: 'A',
+      dataCriacao: '2021-04-29T01:26:35.700Z',
+      multa: 20,
+      status: 1,
+    },
+    {
+      id: 3,
+      nome: 'C',
+      dataCriacao: '2021-04-29T01:26:35.700Z',
+      multa: 30,
+      status: 2,
+    },
+  ];
+
+  const statusApiData = [
+    {
+      id: 1,
+      descricao: 'Ativo',
+    },
+    {
+      id: 2,
+      descricao: 'Inativo',
+    },
+  ];
+
+  beforeEach(() => {
+    vi.spyOn(api, 'get').mockImplementation(async (path: Routes) => {
+      if (path === '/codigopenal') {
+        return [null, { data: codigoPenalApiData }];
+      }
+
+      return [null, { data: statusApiData }];
+    });
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   describe('Table', () => {
     it('should render all fields', () => {
       const { container, render } = createPageContainer();
