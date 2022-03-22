655ba008c56169b0db9e053c82d66456860b6724
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-03-22 03:01:41 -0300

    test(/dashboard): add 'renders only active fields in table' case

--- test/pages/dashboard.test.tsx
+++ test/pages/dashboard.test.tsx
@@ -27,5 +27,32 @@ describe('Dashboard', () => {
         '',
       ]);
     });
+
+    it('should render only active fields', () => {
+      const { container, render, interact } = createPageContainer();
+
+      render(Dashboard);
+      const nonFirstCheckboxList = container.querySelectorAll(
+        '*:nth-child(1n+2) > input[type=checkbox]',
+      );
+      nonFirstCheckboxList.forEach((checkbox) =>
+        interact.change(checkbox, {
+          target: { checked: true },
+        }),
+      );
+
+      const tableHeaderList = Array.from(container.querySelectorAll('th'));
+      const activeTableHeaderList = tableHeaderList.filter(
+        (element) => element.hidden !== true,
+      );
+      expect(tableHeaderList).toHaveLength(5);
+      expect(activeTableHeaderList).toHaveLength(2);
+      expect(
+        activeTableHeaderList.map((element) => element.textContent),
+      ).toEqual([
+        'Nome', //
+        '',
+      ]);
+    });
   });
 });
